edd626da42add62460c6233b55296deed97af482
Michael <mike@mike724.com>
2013-06-06 21:36:13 -0400

    Messages for game end, broadcast method

--- src/main/java/com/mike724/teamjug/game/Game.java
+++ src/main/java/com/mike724/teamjug/game/Game.java
@@ -4,6 +4,7 @@ import com.mike724.teamjug.TeamJug;
 import com.mike724.teamjug.teams.TeamManager;
 import com.mike724.teamjug.timing.TimeConstants;
 import com.mike724.teamjug.timing.Timer;
+import org.bukkit.ChatColor;
 
 @SuppressWarnings("unused")
 public class Game {
@@ -31,7 +32,24 @@ public class Game {
         if(reason != GameEndReason.TIME_OVER) {
             endGameTimer.setKillFlag(true);
         }
+
+        String bcast = "";
+        switch(reason) {
+            case TIME_OVER: bcast = "Time has ran out!"; break;
+            case RED_WIN: bcast = "The "+ChatColor.RED+"RED"+ChatColor.WHITE+" team has won!"; break;
+            case BLUE_WIN: bcast = "The "+ChatColor.BLUE+"BLUE"+ChatColor.WHITE+" team has won!"; break;
+            case MIN_PLAYERS: bcast = "A minimum of "+ChatColor.YELLOW+GameConstants.MIN_PLAYERS+ChatColor.WHITE+" player(s) are required.";
+        }
+        this.broadcast(ChatColor.ITALIC+bcast);
+        this.broadcast("Game ending...");
+
+
+
         tm.emptyTeams();
+    }
+
+    public void broadcast(String msg) {
+        TeamJug.getInstance().getServer().broadcastMessage(ChatColor.DARK_RED+"[TeamJug] "+ChatColor.WHITE+msg);
     }
 
 }
---
+++ src/main/java/com/mike724/teamjug/game/GameConstants.java
@@ -0,0 +1,7 @@
+package com.mike724.teamjug.game;
+
+public class GameConstants {
+
+    public static final int MIN_PLAYERS = 5;
+
+}
